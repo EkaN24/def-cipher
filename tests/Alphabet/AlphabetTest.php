bd4df70636d5e5b200d9eb313af1554dfa83e98d
andrew-kamenchuk <andrew.kamenchuk@gmail.com>
2016-07-23 20:16:10 +0300

    alphabet changes

--- tests/Alphabet/AlphabetTest.php
+++ tests/Alphabet/AlphabetTest.php
@@ -2,23 +2,19 @@
 namespace def\Cipher\Test\Alphabet;
 
 use def\Cipher\Alphabet\Alphabet;
+use def\Cipher\Alphabet\EnglishAlphabet;
+use def\Cipher\Alphabet\UkrainianAlphabet;
 
 class AlphabetTest extends AbstractAlphabetInterfaceTest
 {
-    private static $ukrainian = [
-        'а', 'б', 'в', 'г', 'ґ', 'д','е', 'є', 'ж', 'з',
-        'и', 'і', 'ї', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р',
-        'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ю', 'я',
-    ];
-
     /**
      * {@inheritdoc}
      */
     public function getAlphabets()
     {
         return [
-            [ new Alphabet(range('a', 'z')) ],
-            [ new Alphabet(self::$ukrainian) ],
+            [ new EnglishAlphabet ],
+            [ new UkrainianAlphabet ],
             [ new Alphabet([]) ],
             [ new Alphabet(range('d', 'f')) ],
         ];
